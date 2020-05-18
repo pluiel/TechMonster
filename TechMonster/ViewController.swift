5f1070d7448d838f490a0be8a73a63faeda9e61c
pluiel <pluiel@outlook.com>
2020-05-18 16:20:36 -0700

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by Yuka Uematsu on 2020/05/18.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
